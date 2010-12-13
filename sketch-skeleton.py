4c987ad81f22e95be3f3dc548d670d0adb90ead9
Michal Migurski <mike@stamen.com>
2010-12-13 10:47:23 -0800

    Replaced old bag of rays with new collection of collisions.

--- sketch-skeleton.py
+++ sketch-skeleton.py
@@ -5,19 +5,42 @@ from PIL import Image
 from PIL.ImageDraw import ImageDraw
 from shapely.geometry import Polygon, LineString
 
-def draw_edge(edge, img):
+def draw_edge(edge, img, drawn):
     """
     """
+    if edge in drawn:
+        return
+    
     draw = ImageDraw(img)
     draw.line([(edge.p1.x, edge.p1.y), (edge.p2.x, edge.p2.y)], fill=(0xCC, 0xCC, 0xCC), width=2)
     
-    return img
+    drawn.add(edge)
 
-def draw_ray(ray, img):
+def draw_collision(collision, img, drawn):
     """
     """
-    for tail in set((ray.p_tail, ray.n_tail)):
-        draw_tail(tail, img)
+    if collision in drawn:
+        return
+
+    draw_ray(collision.p_ray, img, drawn)
+    draw_ray(collision.n_ray, img, drawn)
+    
+    p = collision.point
+    
+    draw = ImageDraw(img)
+    draw.line([(p.x - 2, p.y - 2), (p.x + 2, p.y + 2)], fill=(0xCC, 0x00, 0x00), width=1)
+    draw.line([(p.x - 2, p.y + 2), (p.x + 2, p.y - 2)], fill=(0xCC, 0x00, 0x00), width=1)
+    
+    drawn.add(collision)
+
+def draw_ray(ray, img, drawn):
+    """
+    """
+    if ray in drawn:
+        return
+    
+    for tail in (ray.p_tail, ray.n_tail):
+        draw_tail(tail, img, drawn)
     
     p1 = ray.start
     p2 = Point(p1.x + 10 * cos(ray.theta), p1.y + 10 * sin(ray.theta))
@@ -26,13 +49,19 @@ def draw_ray(ray, img):
     draw.rectangle([(p1.x - 1, p1.y - 1), (p1.x + 1, p1.y + 1)], fill=(0xCC, 0x00, 0x00))
     draw.line([(p1.x, p1.y), (p2.x, p2.y)], fill=(0xCC, 0x00, 0x00), width=1)
     
-    return img
+    drawn.add(ray)
 
-def draw_tail(tail, img):
+def draw_tail(tail, img, drawn):
     """
     """
+    if tail in drawn:
+        return
+    
+    for edge in (tail.p_edge, tail.n_edge):
+        draw_edge(edge, img, drawn)
+    
     for other in tail.tails:
-        img = draw_tail(other, img)
+        draw_tail(other, img, drawn)
     
     p1 = tail.start
     p2 = tail.end
@@ -40,7 +69,7 @@ def draw_tail(tail, img):
     draw = ImageDraw(img)
     draw.line([(p1.x, p1.y), (p2.x, p2.y)], fill=(0x00, 0x99, 0xFF), width=1)
     
-    return img
+    drawn.add(tail)
 
 class Point:
     """
@@ -50,7 +79,7 @@ class Point:
         self.y = y
 
     def __repr__(self):
-        return 'Point ' + ('%x (%.1f, %.1f)' % (id(self), self.x, self.y))[4:]
+        return 'Point ' + ('%x (%.1f, %.1f)' % (id(self), self.x, self.y))[2:]
 
 class Edge:
     """
@@ -78,7 +107,7 @@ class Tail:
         self.length += hypot(end.x - self.start.x, end.y - self.start.y)
 
     def __repr__(self):
-        return 'Tail ' + ('%x (%.1f, %s)' % (id(self), self.length, self.is_leaf()))[4:]
+        return 'Tail ' + ('%x (%.1f, %s)' % (id(self), self.length, self.is_leaf()))[2:]
 
     def is_leaf(self):
         return len(self.tails) <= 1
@@ -96,7 +125,7 @@ class Stub:
         self.tails = []
 
     def __repr__(self):
-        return 'Stub ' + ('%x' % id(self))[4:]
+        return 'Stub ' + ('%x' % id(self))[2:]
 
     def is_leaf(self):
         return True
@@ -111,7 +140,7 @@ class Ray:
         self.theta = self._theta()
 
     def __repr__(self):
-        return 'Ray ' + ('%x' % id(self))[4:]
+        return 'Ray ' + ('%x' % id(self))[2:]
 
     def _theta(self):
         n_theta = self.n_tail.n_edge.theta
@@ -130,6 +159,66 @@ class Ray:
             rotation -= pi * 2
         
         return p_theta + rotation/2 + pi/2
+
+class Collision:
+    """
+    """
+    def __init__(self, p_ray, n_ray):
+        self.p_ray = p_ray
+        self.n_ray = n_ray
+        
+        self.distance, self.point = self._intersection()
+
+    def __repr__(self):
+        return 'Collision ' + ('%x' % id(self))[2:]
+
+    def __hash__(self):
+        return id(self)
+
+    def __cmp__(self, other):
+        return cmp(self.distance, other.distance)
+    
+    def _intersection(self):
+        """
+        """
+        ray1, ray2 = self.p_ray, self.n_ray
+        
+        is_closure = ray1.p_tail.p_edge is ray2.n_tail.n_edge and ray2.p_tail.p_edge is ray1.n_tail.n_edge
+        
+        if is_closure:
+        
+            tails1 = set((ray1.p_tail, ray1.n_tail))
+            tails2 = set((ray2.p_tail, ray2.n_tail))
+            
+            if len(tails2) > len(tails1):
+                ray = ray2
+            else:
+                ray = ray1
+        
+            return 0, ray.start
+        
+        x1, y1 = ray1.start.x, ray1.start.y
+        sin1, cos1 = sin(ray1.theta), cos(ray1.theta)
+    
+        x2, y2 = ray2.start.x, ray2.start.y
+        sin2, cos2 = sin(ray2.theta), cos(ray2.theta)
+        
+        # Based on parametric form, where:
+        #   x = x1 + cos1 * t
+        #   y = y1 + sin1 * t
+        #   x = x2 + cos2 * t
+        #   y = y2 + sin2 * t
+        
+        x = (cos2*sin1*x1 - cos2*cos1*y1 - cos1*sin2*x2 + cos1*cos2*y2) / (cos2*sin1 - cos1*sin2)
+        y = (sin2*x - sin2*x2 + cos2*y2) / cos2
+        
+        p1, p2 = ray1.p_tail.p_edge.p1, ray1.p_tail.p_edge.p2
+        x1, y1, x2, y2 = p1.x, p1.y, p2.x, p2.y
+        
+        # see formula 14, http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
+        d = abs((x2 - x1) * (y1 - y) - (x1 - x) * (y2 - y1)) / hypot(x2 - x1, y2 - y1)
+    
+        return d, Point(x, y)
 
 def polygon_edges(poly):
     """
@@ -156,9 +245,9 @@ def edge_rays(edges):
     """
     stubs, rays = [], []
     
-    for (j, edge) in enumerate(edges):
-        i = (j - 1) % len(edges)
-        stub = Stub(edge.p1, edges[i], edges[j])
+    for (i, edge) in enumerate(edges):
+        j = (i + 1) % len(edges)
+        stub = Stub(edge.p2, edges[i], edges[j])
         stubs.append(stub)
 
     for stub in stubs:
@@ -167,54 +256,20 @@ def edge_rays(edges):
 
     return rays
 
+def ray_collisions(rays):
+    """
+    """
+    collisions = []
+    
+    for (i, ray1) in enumerate(rays):
+        j = (i + 1) % len(rays)
+        ray2 = rays[j]
+        collision = Collision(ray1, ray2)
+        collisions.append(collision)
+    
+    return collisions
+
 calculated_intersections = {}
-
-def rays_intersection(ray1, ray2):
-    """
-    """
-    if (ray1, ray2) in calculated_intersections:
-        return calculated_intersections[(ray1, ray2)]
-    
-    is_closure = ray1.p_tail.p_edge is ray2.n_tail.n_edge and ray2.p_tail.p_edge is ray1.n_tail.n_edge
-    
-    if is_closure:
-    
-        tails1 = set((ray1.p_tail, ray1.n_tail))
-        tails2 = set((ray2.p_tail, ray2.n_tail))
-        
-        if len(tails2) > len(tails1):
-            ray = ray2
-        else:
-            ray = ray1
-    
-        return 0, ray.start, ray1, ray2
-    
-    x1, y1 = ray1.start.x, ray1.start.y
-    sin1, cos1 = sin(ray1.theta), cos(ray1.theta)
-
-    x2, y2 = ray2.start.x, ray2.start.y
-    sin2, cos2 = sin(ray2.theta), cos(ray2.theta)
-    
-    # Based on parametric form, where:
-    #   x = x1 + cos1 * t
-    #   y = y1 + sin1 * t
-    #   x = x2 + cos2 * t
-    #   y = y2 + sin2 * t
-    
-    x = (cos2*sin1*x1 - cos2*cos1*y1 - cos1*sin2*x2 + cos1*cos2*y2) / (cos2*sin1 - cos1*sin2)
-    y = (sin2*x - sin2*x2 + cos2*y2) / cos2
-    
-    p1, p2 = ray1.p_tail.p_edge.p1, ray1.p_tail.p_edge.p2
-    x1, y1, x2, y2 = p1.x, p1.y, p2.x, p2.y
-    
-    # see formula 14, http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
-    d = abs((x2 - x1) * (y1 - y) - (x1 - x) * (y2 - y1)) / hypot(x2 - x1, y2 - y1)
-
-    intersection = d, Point(x, y), ray1, ray2
-    
-    calculated_intersections[(ray1, ray2)] = intersection
-    
-    return intersection
 
 def print_rays(rays):
     """
@@ -227,15 +282,6 @@ def print_rays(rays):
             print ' ', tail
             print '   ', tail.end
 
-def paired(things):
-    """
-    """
-    for (i, thing1) in enumerate(things):
-        j = (i + 1) % len(things)
-        thing2 = things[j]
-        
-        yield (thing1, thing2)
-
 if __name__ == '__main__':
     
     poly = Polygon(((150, 25), (250, 250), (50, 250)))
@@ -246,53 +292,55 @@ if __name__ == '__main__':
     
     edges = polygon_edges(poly)
     rays = edge_rays(edges)
+    collisions = ray_collisions(rays)
+    
+    print len(edges), 'edges,', len(rays), 'rays,', len(collisions), 'collisions.'
     
     frame = 1
     
     img = Image.new('RGB', (300, 300), (0xFF, 0xFF, 0xFF))
+    drawn = set()
     
-    for edge in edges:
-        img = draw_edge(edge, img)
-
-    for ray in rays:
-        img = draw_ray(ray, img)
+    for collision in collisions:
+        draw_collision(collision, img, drawn)
     
     img.save('skeleton-%03d.png' % frame)
 
     #print_rays(rays)
     print frame, '-' * 40
     
-    while len(rays) > 1:
+    while len(collisions) > 1:
 
         frame += 1
-    
-        intersections = [rays_intersection(ray1, ray2) for (ray1, ray2) in paired(rays)]
-        intersections = sorted(intersections)
-        distance, point, ray1, ray2 = intersections[0]
         
-        p_tail = Tail(point, ray1.p_tail.p_edge, ray1.n_tail.n_edge, ray1.p_tail, ray1.n_tail)
-        n_tail = Tail(point, ray2.p_tail.p_edge, ray2.n_tail.n_edge, ray2.p_tail, ray2.n_tail)
-        nu_ray = Ray(point, p_tail, n_tail)
+        collisions.sort()
+        collision = collisions.pop(0)
         
-        nu_rays = []
+        p_ray = collision.p_ray
+        n_ray = collision.n_ray
+        point = collision.point
         
-        for ray in rays:
-            if ray is ray1:
-                nu_rays.append(nu_ray)
-            elif ray is ray2:
-                pass
-            else:
-                nu_rays.append(ray)
-    
-        rays, old_rays = nu_rays, rays
+        p_tail = Tail(point, p_ray.p_tail.p_edge, p_ray.n_tail.n_edge, p_ray.p_tail, p_ray.n_tail)
+        n_tail = Tail(point, n_ray.p_tail.p_edge, n_ray.n_tail.n_edge, n_ray.p_tail, n_ray.n_tail)
+        new_ray = Ray(point, p_tail, n_tail)
+        
+        for old_collision in collisions:
+            if old_collision.n_ray is p_ray:
+                new_collision = Collision(old_collision.p_ray, new_ray)
+                collisions.remove(old_collision)
+                collisions.append(new_collision)
+            
+        for old_collision in collisions:
+            if old_collision.p_ray is n_ray:
+                new_collision = Collision(new_ray, old_collision.n_ray)
+                collisions.remove(old_collision)
+                collisions.append(new_collision)
     
         img = Image.new('RGB', (300, 300), (0xFF, 0xFF, 0xFF))
+        drawn = set()
         
-        for edge in edges:
-            img = draw_edge(edge, img)
-    
-        for ray in rays:
-            img = draw_ray(ray, img)
+        for collision in collisions:
+            draw_collision(collision, img, drawn)
         
         img.save('skeleton-%03d.png' % frame)
     
