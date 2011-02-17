2e4b43fbd3f5d98cc1eb73f135258171c62f7c6c
Schuyler Erle <sderle@vishnu.tridity.org>
2011-02-17 02:10:50 -0500

    Make tests work. The results seem a bit off, though.

--- CGAL/skeletron/__init__.py
+++ CGAL/skeletron/__init__.py
@@ -1,14 +1,30 @@
-from _skeletron import skeleton
+import _skeletron
 try:
     from shapely.ops import linemerge
 except ImportError:
     linemerge = None
-    
 
 class InteriorSkeleton(object):
+    """
+    >>> shape = (((-1.0,-1.0), (0.0,-12.0), (1.0,-1.0), (12.0,0.0), \
+		  (1.0,1.0), (0.0,12.0), (-1.0,1.0), (-12.0,0.0)),)
+    >>> skel = InteriorSkeleton(shape)
+    >>> len(skel.outer_bisectors)
+    16
+    >>> len(skel.inner_bisectors)
+    2
+    >>> len(skel.bisectors)
+    18
+    >>> len(skel.boundary) == len(shape[0]) * 2
+    True
+    >>> lines = skel.lines()
+    >>> type(lines) 
+    <class 'shapely.geometry.linestring.LineString'>
+    """
+
     def __init__(self, polygon):
 	if hasattr(polygon, "__geo_interface__"):
-	    geometry = polygon.__geo_interface__()
+	    geometry = polygon.__geo_interface__
 	    if geometry["type"] != "Polygon":
 		raise TypeError("Geometry object must be of polygon type")
 	    edges = geometry["coordinates"]
@@ -18,7 +34,7 @@ class InteriorSkeleton(object):
 	    raise TypeError("Geometry must be iterable or provide a __geo_interface__ method")
 	self.edges, self.inner, self.outer = [], [], []
 	for start, end, edge_type in _skeletron.skeleton(edges):
-	    (self.edges, self.inner, self.outer)[edge_type].append((start, end))
+	    (self.edges, self.outer, self.inner)[edge_type].append((start, end))
 	
     @property
     def bisectors(self):
@@ -40,3 +56,7 @@ class InteriorSkeleton(object):
 	if linemerge is None:
 	    raise ImportError("linestrings() method requires Shapely support")
 	return linemerge(self.inner_bisectors)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
