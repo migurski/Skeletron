59d7f823749811cdb83d4e67ba2738a1b2687116
Schuyler Erle <sderle@vishnu.tridity.org>
2011-02-16 19:06:44 -0500

    Port the dumper to work with polygons with interior holes.

--- CGAL/dump-skeleton.cpp
+++ CGAL/dump-skeleton.cpp
@@ -4,8 +4,8 @@
 #include<boost/shared_ptr.hpp>
 
 #include<CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include<CGAL/Polygon_2.h>
-#include<CGAL/create_straight_skeleton_2.h>
+#include<CGAL/Polygon_with_holes_2.h>
+#include<CGAL/create_straight_skeleton_from_polygon_with_holes_2.h>
 
 #include "print.h"
 
@@ -20,18 +20,8 @@ typedef boost::shared_ptr<Ss> SsPtr ;
 
 int main()
 {
-  Polygon outer ;
-  
-  /*
-  poly.push_back( Point(-1,-1) ) ;
-  poly.push_back( Point(0,-12) ) ;
-  poly.push_back( Point(1,-1) ) ;
-  poly.push_back( Point(12,0) ) ;
-  poly.push_back( Point(1,1) ) ;
-  poly.push_back( Point(0,12) ) ;
-  poly.push_back( Point(-1,1) ) ;
-  poly.push_back( Point(-12,0) ) ;
-  */
+  Polygon outer;
+
   std::string line;
   double x, y;
   while (!getline(std::cin,line).eof()) {
@@ -43,18 +33,9 @@ int main()
     outer.push_back(Point(x, y));
   }
 
-  // You can pass the polygon via an iterator pair
-  //Polygon_with_holes poly(outer);
-  SsPtr iss = CGAL::create_interior_straight_skeleton_2(outer);
+  Polygon_with_holes poly(outer);
+  SsPtr iss = CGAL::create_interior_straight_skeleton_2(poly);
 
-  // Or you can pass the polygon directly, as below.
-  
-  // To create an exterior straight skeleton you need to specify a maximum offset.
-  // double lMaxOffset = 5 ; 
-  // SsPtr oss = CGAL::create_exterior_straight_skeleton_2(lMaxOffset, poly);
-  
   print_skeleton_inner_bisectors(*iss);
-  // print_straight_skeleton(*oss);
-  
   return 0;
 }
