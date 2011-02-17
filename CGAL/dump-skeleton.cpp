a8197236188eed4d1c94a2a31ab963b2edbd26e2
Schuyler Erle <sderle@vishnu.tridity.org>
2011-02-16 19:12:34 -0500

    Add support for reading holes.

--- CGAL/dump-skeleton.cpp
+++ CGAL/dump-skeleton.cpp
@@ -1,5 +1,6 @@
 #include<string>
 #include<iostream>
+#include<vector>
 
 #include<boost/shared_ptr.hpp>
 
@@ -21,21 +22,31 @@ typedef boost::shared_ptr<Ss> SsPtr ;
 int main()
 {
   Polygon outer;
+  std::vector<Polygon *> inners;
 
   std::string line;
   double x, y;
+  Polygon *current = &outer;
+
   while (!getline(std::cin,line).eof()) {
     std::istringstream is(line);
+    if (line == "") {
+	current = new Polygon();
+	inners.push_back(current);
+	continue;
+    }
     if (!(is >> x >> y)) {
         std::cerr << "Bad input line: " << line;
         continue;
     }
-    outer.push_back(Point(x, y));
+    current->push_back(Point(x, y));
   }
 
   Polygon_with_holes poly(outer);
-  SsPtr iss = CGAL::create_interior_straight_skeleton_2(poly);
+  for (std::vector<Polygon *>::iterator it = inners.begin(); it != inners.end(); it++)
+    poly.add_hole(**it);
 
+  SsPtr iss = CGAL::create_interior_straight_skeleton_2(poly);
   print_skeleton_inner_bisectors(*iss);
   return 0;
 }
