0bb6f872b35df089ad5decc4a3d0495592f2f30d
Michal Migurski <migurski@geo.example.org>
2011-02-19 10:21:25 -0800

    Converted tabs to 8 spaces per PEP 8 (http://www.python.org/dev/peps/pep-0008/)

--- CGAL/skeletron/__init__.py
+++ CGAL/skeletron/__init__.py
@@ -41,7 +41,7 @@ except ImportError:
 class InteriorSkeleton(object):
     """
     >>> shape = (((-1.0,-1.0), (0.0,-12.0), (1.0,-1.0), (12.0,0.0), \
-		  (1.0,1.0), (0.0,12.0), (-1.0,1.0), (-12.0,0.0)),)
+                  (1.0,1.0), (0.0,12.0), (-1.0,1.0), (-12.0,0.0)),)
     >>> skel = InteriorSkeleton(shape)
     >>> len(skel.outer_bisectors)
     16
@@ -57,39 +57,39 @@ class InteriorSkeleton(object):
     """
 
     def __init__(self, polygon):
-	if hasattr(polygon, "__geo_interface__"):
-	    geometry = polygon.__geo_interface__
-	    if geometry["type"] != "Polygon":
-		raise TypeError("Geometry object must be of polygon type")
-	    edges = geometry["coordinates"]
-	elif hasattr(polygon, "__iter__"):
-	    edges = polygon
-	else:
-	    raise TypeError("Geometry must be iterable or provide a __geo_interface__ method")
-	self.edges, self.inner, self.outer = [], [], []
-	for start, end, edge_type in _skeletron.skeleton(edges):
-	    (self.edges, self.outer, self.inner)[edge_type].append((start, end))
-	
+        if hasattr(polygon, "__geo_interface__"):
+            geometry = polygon.__geo_interface__
+            if geometry["type"] != "Polygon":
+                raise TypeError("Geometry object must be of polygon type")
+            edges = geometry["coordinates"]
+        elif hasattr(polygon, "__iter__"):
+            edges = polygon
+        else:
+            raise TypeError("Geometry must be iterable or provide a __geo_interface__ method")
+        self.edges, self.inner, self.outer = [], [], []
+        for start, end, edge_type in _skeletron.skeleton(edges):
+            (self.edges, self.outer, self.inner)[edge_type].append((start, end))
+        
     @property
     def bisectors(self):
-	return self.inner + self.outer
+        return self.inner + self.outer
 
     @property
     def inner_bisectors(self):
-	return self.inner
+        return self.inner
 
     @property
     def outer_bisectors(self):
-	return self.outer
+        return self.outer
 
     @property
     def boundary(self):
-	return self.edges
+        return self.edges
 
     def lines(self):
-	if linemerge is None:
-	    raise ImportError("linestrings() method requires Shapely support")
-	return linemerge(self.inner_bisectors)
+        if linemerge is None:
+            raise ImportError("linestrings() method requires Shapely support")
+        return linemerge(self.inner_bisectors)
 
 if __name__ == "__main__":
     import doctest
