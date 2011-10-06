adfc56b16bf8de7afc9b53b022142a2f5bb259aa
Michal Migurski <migurski@geo.example.org>
2011-10-06 14:48:52 -0700

    A script for converting from .osm to geoJSON

---
+++ osm-to-json.py
@@ -0,0 +1,35 @@
+from sys import stdin, argv
+from math import hypot, ceil
+from json import dump
+
+from shapely.geometry import Polygon, MultiLineString
+
+from Skeletron2 import ParserOSM, Canvas, network_polygon, polygon_rings, polygon_skeleton, skeleton_routes, simplify_line, merc
+
+p = ParserOSM()
+g = p.parse(stdin.read())
+
+output = dict(type='FeatureCollection', features=[])
+
+for key in g:
+    print key
+    network = g[key]
+    
+    if not network.edges():
+        continue
+    
+    poly = network_polygon(network)
+    skeleton = polygon_skeleton(poly)
+    routes = skeleton_routes(skeleton)
+    
+    if not routes:
+        continue
+    
+    coords = [[merc(*point, inverse=True) for point in route] for route in routes]
+    geometry = MultiLineString(coords).__geo_interface__
+    properties = dict(name=key[0], highway=key[1])
+    
+    feature = dict(geometry=geometry, properties=properties)
+    output['features'].append(feature)
+
+dump(output, open(argv[2], 'w'))
