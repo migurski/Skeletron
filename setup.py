16e454190346a370405a42d131803429452681e9
Michal Migurski <migurski@geo.example.org>
2011-10-10 15:21:29 -0700

    Preparing a primitive package

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = open('VERSION', 'r').read().strip()
+
+setup(name='Skeletron',
+      version=version,
+      description='Generalizes collections of lines on maps to simpler lines for improved labeling.',
+      author='Michal Migurski',
+      author_email='mike@stamen.com',
+      url='https://github.com/migurski/Skeletron',
+      requires=['networkx'],
+      packages=['Skeletron'],
+      scripts=[],
+    # download_url='https://github.com/downloads/migurski' % locals(),
+      license='BSD')
