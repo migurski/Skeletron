a893a4aa95f164a5945289e8849fa2bfcc54e556
Michal Migurski <mike@stamen.com>
2013-01-13 17:17:00 -0800

    Upgraded skeletron-generalize to accept complete command line options and arguments

--- Skeletron/output.py
+++ Skeletron/output.py
@@ -1,12 +1,38 @@
 from pickle import dumps as pickleit
 from tempfile import mkstemp
 from os import write, close
+from json import dumps
 
 import logging
 
-from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import LineString, MultiLineString, asShape
 
-from . import multigeom_centerline, mercator, _GraphRoutesOvertime
+from . import multigeom_centerline, mercator, _GraphRoutesOvertime, projected_multigeometry
+from .util import zoom_buffer
+
+def generalize_geojson_feature(feature, width, zoom):
+    ''' Run one GeoJSON features through Skeletron and return it.
+    
+        If generalization fails, return False.
+    '''
+    prop = dict([(k.lower(), v) for (k, v) in feature['properties'].items()])
+    name = prop.get('name', prop.get('id', prop.get('gid', prop.get('fid', None))))
+    geom = asShape(feature['geometry'])
+
+    buffer = zoom_buffer(width, zoom)
+    kwargs = dict(buffer=buffer, density=buffer/2, min_length=8*buffer, min_area=(buffer**2)/4)
+    
+    logging.info('Generalizing %s, %d wkb, %.1f buffer' % (dumps(name), len(geom.wkb), buffer))
+    
+    multigeom = projected_multigeometry(geom)
+    generalized = generalized_multiline(multigeom, **kwargs)
+    
+    if generalized is None:
+        return False
+    
+    feature['geometry'] = generalized.__geo_interface__
+    
+    return feature
 
 def multilines_geojson(multilines, key_properties, buffer, density, min_length, min_area):
     """
--- skeletron-generalize.py
+++ skeletron-generalize.py
@@ -1,36 +1,49 @@
 from json import load, dump
+from optparse import OptionParser
+import logging
 
-from shapely.geometry import asShape
-
-from Skeletron import projected_multigeometry
-from Skeletron.output import generalized_multiline
-from Skeletron.util import zoom_buffer
+from Skeletron.output import generalize_geojson_feature
 
 earth_radius = 6378137
 
-def generalize(feature, width, zoom):
-    '''
-    '''
-    buffer = zoom_buffer(width, zoom)
-    
-    kwargs = dict(buffer=buffer, density=buffer/2, min_length=8*buffer, min_area=(buffer**2)/4)
-    
-    geom = asShape(feature['geometry'])
-    multigeom = projected_multigeometry(geom)
-    generalized = generalized_multiline(multigeom, **kwargs)
-    
-    if generalized is None:
-        return False
-    
-    feature['geometry'] = generalized.__geo_interface__
-    
-    return feature
+optparser = OptionParser(usage="""%prog [options] <geojson input file> <geojson output file>
+
+Accepts GeoJSON input and generates GeoJSON output.""")
+
+defaults = dict(zoom=12, width=15, loglevel=logging.INFO)
+
+optparser.set_defaults(**defaults)
+
+optparser.add_option('-z', '--zoom', dest='zoom',
+                     type='int', help='Zoom level. Default value is %s.' % repr(defaults['zoom']))
+
+optparser.add_option('-w', '--width', dest='width',
+                     type='float', help='Line width at zoom level. Default value is %s.' % repr(defaults['width']))
+
+optparser.add_option('-v', '--verbose', dest='loglevel',
+                     action='store_const', const=logging.DEBUG,
+                     help='Output extra progress information.')
+
+optparser.add_option('-q', '--quiet', dest='loglevel',
+                     action='store_const', const=logging.WARNING,
+                     help='Output no progress information.')
 
 if __name__ == '__main__':
 
-    geojson = load(open('oakland-sample.json'))
+    options, (input_file, output_file) = optparser.parse_args()
+
+    logging.basicConfig(level=options.loglevel, format='%(levelname)08s - %(message)s')
     
-    features = [generalize(feature, 15, 15) for feature in geojson['features']]
-    geojson['features'] = filter(None, features)
+    #
+    # Input
+    #
     
-    dump(geojson, open('output.json', 'w'))
+    input = load(open(input_file, 'r'))
+    features = [generalize_geojson_feature(feature, options.width, options.zoom) for feature in input['features']]
+    
+    #
+    # Output
+    #
+    
+    output = dict(type='FeatureCollection', features=filter(None, features))
+    dump(output, open(output_file, 'w'))
